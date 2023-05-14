71c943873d6bdb58d0c1ce8765955012b99bfb8f
Bader Mohammad <bader97alyami@hotmail.com>
2023-05-14 11:49:15 +0300

    update

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,13 @@ import { log } from "console";
 
 
 const app: Application = express();
-// app.use(express.json());
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb' }));
-app.use(cors());
-app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ limit: '50mb' }));
+app.use(cors());
+
 dotenv.config();
 connectDB();
 
